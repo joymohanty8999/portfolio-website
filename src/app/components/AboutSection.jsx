4296ce8c8125d099627385209b6793fe952896b2
Joseph Mohanty <jm215@rice.edu>
2024-06-17 23:27:41 -0500

    Adding alt to Image in  About Section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -7,6 +7,7 @@ const AboutSection = () => {
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image 
                     src = "/images/about-me.png"
+                    alt= "about-me"
                     width={600}
                     height={600}
                 />
