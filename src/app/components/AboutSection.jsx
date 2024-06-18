ad6893c121d16d567f283084a62cbb13da82ba0e
Joseph Mohanty <jm215@rice.edu>
2024-06-17 23:06:20 -0500

    Adding components

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+
+const AboutSection = () => {
+    return(
+        <section className="text-white" id="about">
+            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+                <Image 
+                    src = "/images/about-me.png"
+                    width={600}
+                    height={600}
+                />
+
+                <div>
+                    <h2 className="text-4xl font-sans font-bold mb-4 text-white">ABOUT ME</h2>
+                    <br/>
+
+                    <p className="text-base lg:text-lg">
+
+                        My name is Joseph Anurag Mohanty. I am currently pursuing my Masters in Computer Science at <a href ="https://www.rice.edu" target="_blank" className="text-blue-500 hover:text-blue-700">Rice University</a>, with a Bachelor’s degree in Computer Science Engineering from <a href="https://www.manipal.edu/mit.html"target="_blank" className="text-blue-500 hover:text-blue-700">Manipal Institute of Technology</a>. I am specializing in the field of Data Science and Machine Learning. With a strong foundation in software development and a keen interest in emerging technologies, I have continually sought out opportunities to apply my skills to real-world challenges.
+
+                    </p>
+
+                    <br/>
+
+                    <p className="text-base lg:text-lg">
+
+                        Currently, I am working as a Research Assistant under <a href="https://www.cs.rice.edu/~cmj4/" target="_blank" className="text-blue-500 hover:text-blue-700">Professor Chris Jermaine</a>. We are working on a Machine Learning system - Einsummable - that automatically distributes ML compute graphs across clusters of CPU or GPU machines. It is based on the idea that tensors are relations mapping positions to values and all operations are therefore relational. The benefit of treating ML compute graphs as relational queries is that they can be easily reasoned and optimized over.
+
+                    </p>
+
+                    <br/>
+
+                    <p className="text-base lg:text-lg">
+                        Prior to joining Rice, I worked as Tech Engineer at <a href="https://www.ubs.com/us/en.html" target="_blank" className="text-blue-500 hover:text-blue-700">UBS</a> in their Investment Banking Division, where I was part of the CAIP (Corporate Actions and Income Processsing) Team. I primarily contributed to leading code release activities for our application suite.
+                    </p>
+
+                </div>
+
+            </div>
+        </section>
+        
+    )
+}
+
+export default AboutSection
---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+    return (
+        <section className="hero-section">
+            <div className="grid grid-cols-1 sm:grid-cols-12 items-center lg:gap-8 gap-4">
+                <div className="col-span-1 sm:col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-2 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-500">
+                            Hello, I'm
+                        </span>
+                        <br />
+                        <span className="block sm:inline-block" style={{ minHeight: '1.5em' }}>
+                            <TypeAnimation
+                                sequence={['Joseph Mohanty', 2000, 
+                                    'a Software Developer', 1500, 
+                                    'a Machine Learning Engineer', 1500,
+                                ]}
+                                repeat={Infinity}
+                                deletionSpeed={90}
+                                className="inline-block"
+                            />
+                        </span>
+                    </h1>
+
+                    <p className="text-[#ADB7BE] text-lg lg:text-xl mb-4">
+                        An enthusiastic Computer Science student, looking for opportunities in the field of Software Development, Data Science and Machine Learning.
+                    </p>
+
+                    <div className="flex flex-col sm:flex-row justify-center sm:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
+                        <button className="px-6 py-3 w-full sm:w-auto rounded-full bg-gradient-to-r from-blue-400 to-blue-500 hover:from-blue-500 hover:to-blue-600 text-white">
+                            Download CV
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-1 sm:col-span-5 flex justify-center mt-6 sm:mt-0">
+                    <div className="rounded-full bg-[#e5dddd] w-[80vw] h-[80vw] sm:w-[50vw] sm:h-[50vw] lg:w-[400px] lg:h-[400px] max-w-[400px] max-h-[400px] overflow-hidden flex items-center justify-center relative"
+                        style={{ marginLeft: '20px' }}> 
+                        <Image 
+                            src="/images/portfolio-image.png" 
+                            alt="portfolio-image" 
+                            className="object-cover" 
+                            style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'top' }} 
+                            width={500} height={500}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default HeroSection;
