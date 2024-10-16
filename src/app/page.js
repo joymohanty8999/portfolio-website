5b476debb4eff5a38b7a18bbf500805b8b2ee5c4
Joseph Mohanty <jm215@rice.edu>
2024-10-16 14:26:29 -0500

    Updating Hero Section

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ export default function Home() {
       <NavBar/>
 
       <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection/>
         <AboutSection/>
         <ProjectSection/>
       </div>
