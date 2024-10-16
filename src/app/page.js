632a32e6a35f988bc1df420af05c9844f4c02f98
Joseph Mohanty <jm215@rice.edu>
2024-10-16 14:22:07 -0500

    Update Resume

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,6 @@ export default function Home() {
       <NavBar/>
 
       <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection/>
         <AboutSection/>
         <ProjectSection/>
       </div>
