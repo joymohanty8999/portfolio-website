d5962ab2363ddb38b697a648e1b2cb99abdbc658
Joseph Mohanty <jm215@rice.edu>
2024-12-18 20:19:20 -0600

    Editing pdf paths

--- src/app/components/PublicationSection.jsx
+++ src/app/components/PublicationSection.jsx
@@ -13,7 +13,7 @@ const PublicationData = [
         ],
         abstract: "The #MeToo trend has led to people talking about personal experiences of harassment more openly. This work attempts to aggregate such experiences of sexual abuse to facilitate a better understanding of social media constructs and to bring about social change. We propose an approach to multimodal sentiment analysis using deep neural networks combining visual analysis and natural language processing. Our goal is different than the standard sentiment analysis goal of predicting whether a sentence expresses positive or negative sentiment; instead we try to detect the stand of a person on the topic and deduce the emotions conveyed. We have made use of a Multimodal Bi-Transformer (MMBT) model which combines both image and text features to produce an optimal prediction of a tweet's stand and sentiments on the #MeToo campaign.",
         link: "https://ieeexplore.ieee.org/document/9232518",
-        pdf: "public/files/Publication/Multimodal_Sentiment_Analysis_of_MeToo_Tweets_using_Focal_Loss_Grand_Challenge.pdf"
+        pdf: "files/Publication/Multimodal_Sentiment_Analysis_of_MeToo_Tweets_using_Focal_Loss_Grand_Challenge.pdf"
     }
 ];
 
