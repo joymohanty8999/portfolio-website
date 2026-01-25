34c2ba10ebd7a2d1634c42d378bb57d6a75dd1b0
Joseph Mohanty <jm215@rice.edu>
2026-01-25 00:03:09 -0600

    Updating contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -50,13 +50,13 @@ export default function Contact() {
                         <a href="https://www.linkedin.com/in/joy-mohanty" target="_blank" rel="noopener noreferrer">
                             <Image src={LinkedinIcon} alt="Linkedin Icon" className="w-12 h-12" />
                         </a>
-                        <a href="mailto:jm215@rice.edu" target="_blank" rel="noopener noreferrer">
+                        <a href="mailto:josephmohanty8999@gmail.com" target="_blank" rel="noopener noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 text-white">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
                             </svg>
                         </a>
 
-                        <a href="https://leetcode.com/u/joy_mohanty/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://leetcode.com/u/josephmohanty8999/" target="_blank" rel="noopener noreferrer">
                             <Image
                                 src="/logos/leetcode-white.svg"
                                 alt="LeetCode Logo"
